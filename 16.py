c29e2787ceadfa64ac81001d44e33c62378934a5
Sam Davies <sam.davies@metaswitch.com>
2022-12-16 19:32:19 +0000

    16 part 2 - takes ~6min

--- 16.py
+++ 16.py
@@ -50,9 +50,9 @@ dists = {'AA': dijk('AA', list(to_open), nodes)}
 for node in to_open:
     dists[node] = dijk(node, list(to_open), nodes)
 
-# main algo: some kind of modified djikstra? cost(/benefit?) of next node is (30 - now - travel_time)*flow_rate
 # all possible routes is 15! ~= 10**12 - not feasible to search
 # some way to cut early if we can see we can't beat current best?
+# ^ turned out not to be necessary - full DFS was fast enough
 def explore(current: str, remaining: set, time_left: int, will_release: int):
     # current node, remaining nodes with valves, how many minutes remain, total amount of 
     # pressure that will be released by minute 30 with the set of valves that are currently open
@@ -70,4 +70,50 @@ def explore(current: str, remaining: set, time_left: int, will_release: int):
 
     return best
 
-print(explore('AA', to_open, 30, 0))
+print(explore('AA', to_open, 30, 0)) # PART ONE
+
+# PART TWO
+# currently takes ~6 minutes to run
+def explore(first: str, second: str, second_delay: int,  remaining: set, time_left: int, will_release: int):
+    # as before, but now with first=location of first worker, second=destination of second worker, 
+    # second_delay = time until second worker has reached & finished opening the valve at 'second'
+    if not remaining: return will_release
+
+    best = will_release
+    for node in remaining:
+        first_delay = dists[first][node] + 1
+        if first_delay >= time_left:
+            # not enough time for First to get there, open the valve, and something to happen
+            if second_delay >= time_left:
+                continue # nor Second
+            else:
+                # worry: recursion without removing from 'remaining' - although time_left does reduce, so it's ok
+                attempt = explore(second, first, first_delay, remaining, time_left - second_delay, will_release)
+        else:
+            new_remain = set(remaining); new_remain.remove(node)
+            if first_delay <= second_delay:
+                new_first = node
+                new_second = second
+                new_left = time_left - first_delay
+                new_second_delay = second_delay - first_delay
+            else:
+                new_first = second
+                new_second = node
+                new_left = time_left - second_delay
+                new_second_delay = first_delay - second_delay
+            
+            firsts_contribution = (time_left - first_delay)*nodes[node].rate
+            attempt = explore(new_first,new_second,new_second_delay, new_remain, new_left, will_release + firsts_contribution)
+            if attempt > best: best = attempt
+
+    return best
+
+print(explore('AA', 'AA', 0, to_open, 26, 0))
+
+
+
+
+
+
+
+
