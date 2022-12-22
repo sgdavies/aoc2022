f45cc123fbbd73a4bce708e44dd835253085b3fc
Sam Davies <sam.davies@metaswitch.com>
2022-12-22 18:31:27 +0000

    11 part 1 (python)

---
+++ 11.py
@@ -0,0 +1,43 @@
+import sys
+from typing import Callable
+
+class Monkey:
+    items: list
+    op: Callable
+    test: int
+    true: int
+    false: int
+    inspected: int
+
+    def __init__(self, defn: str):
+        lines = defn.split('\n')
+        self.items = [int(x) for x in lines[1].split(':')[1].split(', ')]
+        self.op = eval("lambda old: (" + lines[2].split('=')[1] + ") // 3")
+        self.test = int(lines[3].split(' ')[-1])
+        self.true = int(lines[4].split(' ')[-1])
+        self.false = int(lines[5].split(' ')[-1])
+        self.inspected = 0
+
+monkeys = []
+fname = '11.txt' if len(sys.argv)==1 else sys.argv[1]
+with open(fname) as f:
+    monkeys = [Monkey(s) for s in f.read().split("\n\n")]
+
+
+ROUNDS = 20
+for _round in range(ROUNDS):
+    for monkey in monkeys:
+        while monkey.items:
+            item = monkey.items.pop(0)
+            monkey.inspected += 1
+            newval = monkey.op(item)
+            to = monkeys[monkey.true] if newval % monkey.test == 0 else monkeys[monkey.false]
+            to.items.append(newval)
+
+    if False:
+        print("Round: ", _round)
+        for monkey in monkeys:
+            print(", ".join([str(item) for item in monkey.items]))
+
+active = sorted(monkeys, key=lambda monkey: monkey.inspected, reverse=True)
+print(active[0].inspected * active[1].inspected)
